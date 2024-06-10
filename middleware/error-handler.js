95daddab9526ca9e3a20526fb1c9d7c92ecdd937
franfran20 <rickyegboluche@gmail.com>
2024-06-10 14:20:12 +0100

    first commit

---
+++ middleware/error-handler.js
@@ -0,0 +1,34 @@
+const { custom } = require("joi");
+const CustomAPIError = require("../errors/custom-api");
+const { StatusCodes } = require("http-status-codes");
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || "Something went wrong, try again later",
+  };
+
+  if (err.name == "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name == "CastError") {
+    customError.message = `No job found with id ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.message = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.message });
+};
+
+module.exports = errorHandlerMiddleware;
